4b751b58d2916392eb0b02f858efb273054013f2
Nhicolas Charles <cristian.deluna19@gmail.com>
2024-07-29 11:17:56 +0800

    comment

--- navbar.php
+++ navbar.php
@@ -1,15 +1,16 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-transparent">
+
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img class="rounded-pill" src="images/crc.jpg" alt="" height="40px"></a>
+      <a class="navbar-brand" href="#"><img class="rounded-pill" src="images/crc.jpg" alt="" height="90px"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link text-dark fw-bold" href="index.php#home">Home</a>
-          <a class="nav-link text-dark fw-bold" href="index.php#contact">Contact Us</a>
-          <a class="nav-link text-dark fw-bold" href="tracking.php">Tracking Page</a>
-          <a class="nav-link text-dark fw-bold" href="loginpage.php">Login</a>
+        <div class="navbar-nav ms-auto mb-2 mb-lg-0 fs-5">
+          <a class="nav-link text-white fw-bold" href="index.php#home">Home</a>
+          <a class="nav-link text-white fw-bold" href="index.php#contact">Contact Us</a>
+          <a class="nav-link text-white fw-bold" href="tracking.php">Tracking Page</a>
+          <a class="nav-link text-white fw-bold" href="loginpage.php">Login</a>
         </div>    
       </div>
     </div>
